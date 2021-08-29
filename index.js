e0784a7f644ffff20f7806acc5436debab42ad91
3schools <85482920+3schools@users.noreply.github.com>
2021-08-29 13:47:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
+}
+
+let deferredPrompt;
+var addBtn = document.querySelector(".addbtn");
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  addBtn.addEventListener('click', school);
+  var intervalId = window.setInterval(school, 5000);
+  function school() {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User Download WB 3schools');
+      } else {
+        console.log('User will download WB 3schools');
+      }
+      deferredPrompt = null;
+    });
+    }
+}); 
+
+
+function darkMode() {
+   var elementD = document.body;
+   elementD.classList.toggle("dark-mode");
+} 
+
+function search(string){
+ window.find(string);
+ }
+
+function share() {
+    var text = 'Ask 3schools';
+    if ('share' in navigator) {
+        navigator.share({
+            title: document.title,
+            text: text,
+            url: location.href,
+        })
+    } else {
+        location.href = 'https://api.whatsapp.com/send?text=' + encodeURIComponent(text + ' - ') + location.href
+    }
+}   
