6a26ad7487db6ced8906421e27bbbfd5ec060ad1
3schools <85482920+3schools@users.noreply.github.com>
2021-08-30 11:24:02 +0530

    Update networkChange.js

--- js/networkChange.js
+++ js/networkChange.js
@@ -7,20 +7,17 @@
       window.addEventListener('offline', checkConnectivity, false);
     });    
     function checkConnectivity() {
-      /* 
-      * PWA Starter Demo
-      * Add logic to change colors or theme here to let user know about connectivity changes    
-      */
+      
       if (navigator.onLine) {
         navigator.serviceWorker.ready
         .then(function (registration) {
-          registration.showNotification('You are back online.')
+          registration.showNotification('You are back online....')
         });        
       }
       else {
         navigator.serviceWorker.ready
         .then(function (registration) {
-          registration.showNotification('Network connection lost.')
+          registration.showNotification('Network connection lost....')
         });
       }
     }
