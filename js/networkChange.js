b578b6d3fbafb6408613bbe92e6b15c0aa9f83de
3schools <85482920+3schools@users.noreply.github.com>
2021-09-04 18:21:45 +0530

    Update networkChange.js

--- js/networkChange.js
+++ js/networkChange.js
@@ -16,8 +16,7 @@
           icon: '/images/icon_256.png',
           vibrate: [200, 100, 200, 100, 200, 100, 200],
           tag: 'You are back online',
-          url: 'https://ask.3schools.in'
-             
+          
         }); 
         });        
       }
@@ -25,7 +24,7 @@
         navigator.serviceWorker.ready
         .then(function (registration) {
           registration.showNotification('No Internet', {
-          body: 'Ask 3schools will let you know when this page is ready',
+          body: 'we will let you know when this page is ready',
           icon: '/images/icon_256.png',
           vibrate: [200, 100, 200, 100, 200, 100, 200],
           tag: 'No Internet'
