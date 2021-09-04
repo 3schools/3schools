3e190dde385ed9e89417e0b78b8a6487920a81e2
3schools <85482920+3schools@users.noreply.github.com>
2021-09-04 18:28:01 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,27 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('user3schools').then((cache) => cache.addAll([
-      'index.html',
-      '/images/icon_256.png',
+      './',
+    './index.html',
+    './css/styles.css',
+    '/js/notify.js',
+    '/js/app.js',
+    '/js/sync.js',
+    '/js/push.js',
+    '/js/networkChange.js',
+    './manifest.json',
+     'sw.js',
       'index.js',
-      'style.css',
       'install.html',
       'search.html',
       '404.html',
       'profile.html',
       'serviceWorker.js',
-      'sw.js',
-      'manifest.json',
+    './images/icon_16.png',
+    './images/icon_32.png',
+    './images/icon_192.png',
+    './images/icon_256.png',
+    './images/icon_512.png'
     ])),
   );
 });
