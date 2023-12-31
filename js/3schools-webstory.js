3b9b6c75d0f3492a579721b547bf0f46b29305af
3schools <85482920+3schools@users.noreply.github.com>
2023-12-31 23:31:22 +0530

    Update 3schools-webstory.js

--- js/3schools-webstory.js
+++ js/3schools-webstory.js
@@ -6,23 +6,25 @@ function sLink(l, callback) {
 }
 sLink("https://app.3schools.in/js/3schools-webstory-module.js", function() {
 
-  document.addEventListener('DOMContentLoaded', function() {
+ // document.addEventListener('DOMContentLoaded', function() {
   let startI = 1;
 
   const nextButton = document.querySelector('#next-button');
   console.log(nextButton); // Check if nextButton is null
-
+if(nextButton){
   nextButton.addEventListener('click', () => {
     if (qS('.webstories-3schools')) {
       sLink(`${hP}feeds/pages/default?alt=json&start-index=${startI}&max-results=6&callback=webStory`);
       startI += 6;
     }
   });
+}
+
      if (qS('.webstories-3schools')) {
         sLink(`${hP}feeds/pages/default?alt=json&start-index=${startI}&max-results=6&callback=webStory`);
         startI += 6;
       }
-   });
+  // });
 
     if (qS('.trending-posts.related')) {
       sLink(`${hP}feeds/posts/default/-/${lA[Math.floor(Math.random() * lA.length)]}?alt=json&max-results=${rP.nP}&callback=rePo`);
