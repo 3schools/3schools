e568ba2c1954b2ab27a7b882403ab48b9dfe7b97
3schools <85482920+3schools@users.noreply.github.com>
2023-12-31 23:20:40 +0530

    Update 3schools-webstory.js

--- js/3schools-webstory.js
+++ js/3schools-webstory.js
@@ -5,19 +5,27 @@ function sLink(l, callback) {
   document.body.appendChild(rSl);
 }
 sLink("https://app.3schools.in/js/3schools-webstory-module.js", function() {
-    let startI = 1;
 
-    document.querySelector('#next-button').addEventListener('click', ()=>{
-      if (qS('.webstories-3schools')) {
-        sLink(`${hP}feeds/pages/default?alt=json&start-index=${startI}&max-results=6&callback=webStory`);
-        startI += 6;
-      }
-    })
+  document.addEventListener('DOMContentLoaded', function() {
+  let startI = 1;
+
+  const nextButton = document.querySelector('#next-button');
+  console.log(nextButton); // Check if nextButton is null
+
+  nextButton.addEventListener('click', () => {
+    if (qS('.webstories-3schools')) {
+      sLink(`${hP}feeds/pages/default?alt=json&start-index=${startI}&max-results=6&callback=webStory`);
+      startI += 6;
+    }
+  });
+});
+
+  
      if (qS('.webstories-3schools')) {
         sLink(`${hP}feeds/pages/default?alt=json&start-index=${startI}&max-results=6&callback=webStory`);
         startI += 6;
       }
-   // showStory();
+   
 
     if (qS('.trending-posts.related')) {
       sLink(`${hP}feeds/posts/default/-/${lA[Math.floor(Math.random() * lA.length)]}?alt=json&max-results=${rP.nP}&callback=rePo`);
