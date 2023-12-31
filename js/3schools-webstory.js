59013497040374ae9380e5202077238b5edab6d3
3schools <85482920+3schools@users.noreply.github.com>
2023-12-31 22:44:37 +0530

    Update 3schools-webstory.js

--- js/3schools-webstory.js
+++ js/3schools-webstory.js
@@ -5,26 +5,28 @@ function sLink(l, callback) {
   document.body.appendChild(rSl);
 }
 
-sLink("https://app.3schools.in/js/3schools-webstory-module.js", function() {
-  let startI = 1;
-  
-  function showStory() {
-    if (qS('.webstories-3schools')) {
-      sLink(`${hP}feeds/pages/default?alt=json&start-index=${startI}&max-results=6&callback=webStory`);
-      startI += 6;
-    }
-  }
-  
-  document.querySelector('#next-button').addEventListener('click', showStory);
-  showStory();
+document.addEventListener('DOMContentLoaded', function () {
+  sLink("https://app.3schools.in/js/3schools-webstory-module.js", function() {
+    let startI = 1;
 
-  if (qS('.trending-posts.related')) {
-    sLink(`${hP}feeds/posts/default/-/${lA[Math.floor(Math.random() * lA.length)]}?alt=json&max-results=${rP.nP}&callback=rePo`);
-    window.rePo = (k) => { reP(k, qS('.trending-posts.related')) };
-  }
-  
-  if (qS('.trending-posts.webstories')) {
-    sLink(`${hP}feeds/pages/default?alt=json&max-results=6&callback=wsPo`);
-    window.wsPo = (k) => { reP(k, qS('.trending-posts.webstories')) };
-  }
+    function showStory() {
+      if (qS('.webstories-3schools')) {
+        sLink(`${hP}feeds/pages/default?alt=json&start-index=${startI}&max-results=6&callback=webStory`);
+        startI += 6;
+      }
+    }
+
+    document.querySelector('#next-button').addEventListener('click', showStory);
+    showStory();
+
+    if (qS('.trending-posts.related')) {
+      sLink(`${hP}feeds/posts/default/-/${lA[Math.floor(Math.random() * lA.length)]}?alt=json&max-results=${rP.nP}&callback=rePo`);
+      window.rePo = (k) => { reP(k, qS('.trending-posts.related')) };
+    }
+
+    if (qS('.trending-posts.webstories')) {
+      sLink(`${hP}feeds/pages/default?alt=json&max-results=6&callback=wsPo`);
+      window.wsPo = (k) => { reP(k, qS('.trending-posts.webstories')) };
+    }
+  });
 });
